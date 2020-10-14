dbb41ebb24b0a97e9354890ffd4d8a2e65535c2e
tlefrancois <theodore.lefrancois2906@gmail.com>
2020-10-14 13:37:53 +0200

    add state mode

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,22 +3,37 @@ import PropTypes from 'prop-types';
 import './Contact.css';
 
 
-const Contact =({name, avatar, isOnline})=> (
-    <div className='Contact'>
-        <div className='avatar'>
-            <img  className='avatar' src={avatar} alt="David_COoper_Image"/>
-        </div>
-        <div className='status status-text '>
-            <div className='Name'>
-                {name}
+class Contact extends React.Component{
+    constructor(props){
+        super(props);
+        this.state= {
+            isOnline: false,
+        };
+    }
+    render(){
+        return (
+            <div className='Contact'>
+                <div className='avatar'>
+                    <img  className='avatar' src={this.props.avatar} alt={this.props.avatar}/>
+                </div>
+                <div className='status status-text '>
+                    <div className='Name'>
+                        {this.props.name}
+                    </div>
+                    <div 
+                    onClick={event=>{
+                        const newStatut = !this.state.isOnline;
+                        this.setState({isOnline : newStatut})
+                        }}
+                    className={this.state.isOnline ? 'status-online': 'status-offline' }>
+                        {this.state.isOnline ?  'Online': 'Offline '}
+                    </div>
+                </div>
             </div>
-            <div className={isOnline ? 'status-online': 'status-offline' }>
-                {isOnline ?  'Online': 'Offline '}
-            </div>
-        </div>
-    </div>
+        );
+    }
+}
 
-);
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
